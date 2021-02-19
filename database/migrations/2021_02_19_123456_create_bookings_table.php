29ffbcc5cdaf09b0d9c6108e223118da6ed66dc8
nangirvan <nangirvan98@gmail.com>
2021-02-19 19:39:30 +0700

    Database: Add migrations

---
+++ database/migrations/2021_02_19_123456_create_bookings_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBookingsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('bookings', function (Blueprint $table) {
+            $table->foreignId('id_user')->constrained('users')->onUpdate('cascade')->onDelete('cascade');
+            $table->foreignId('id_task')->constrained('tasks')->onUpdate('cascade')->onDelete('cascade');
+            $table->timestampTz('booked_at');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('bookings');
+    }
+}
