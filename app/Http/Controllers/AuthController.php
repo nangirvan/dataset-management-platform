7e9b13aecd429b033b90048ae0bd232b94c64a7d
nangirvan <nangirvan98@gmail.com>
2021-02-20 10:34:11 +0700

    Auth: Add autentikasi status user pada controller

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -9,11 +9,15 @@ class AuthController extends Controller
 {
     public function index()
     {
-        return view('auth.login');
+        return (auth()->check()) ? redirect()->route('home') : view('auth.login');
     }
 
     public function login(Request $request)
     {
+        if (auth()->check()) {
+            return redirect()->route('home');
+        }
+
         $credentials = $request->only('email', 'password');
 
         if (Auth::attempt($credentials)) {
@@ -28,11 +32,13 @@ class AuthController extends Controller
 
     public function logout(Request $request)
     {
-        Auth::logout();
+        if (auth()->check()) {
+            Auth::logout();
 
-        $request->session()->invalidate();
-        $request->session()->regenerateToken();
-
+            $request->session()->invalidate();
+            $request->session()->regenerateToken();
+        }
+     
         return redirect()->route('auth.login.view');
     }
 }
