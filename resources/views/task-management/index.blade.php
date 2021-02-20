0267c2b97c7831d8bef94028dd949fbaa44a2919
nangirvan <nangirvan98@gmail.com>
2021-02-20 11:15:44 +0700

    Task: Add tabel responsif pada task list

--- resources/views/task-management/index.blade.php
+++ resources/views/task-management/index.blade.php
@@ -14,68 +14,70 @@
             </div>
         </div>
         
-        <table class="table table-borderless">
-            <thead>
-                <tr class="bg-secondary text-white">
-                    <th scope="col">NO</th>
-                    <th scope="col">NAME</th>
-                    <th scope="col">SIZE</th>
-                    <th scope="col">STATUS</th>
-                    <th scope="col">BOOKING</th>
-                    <th scope="col">ACTION</th>
-                </tr>
-            </thead>
-            <tbody class="bg-light">
-                @foreach($tasks as $task)
-                <tr class="align-middle">
-                    <td>
-                        {{ $loop->iteration }}
-                    </td>
-                    <td>
-                        <strong>{{ $task->name }}</strong>
-                    </td>
-                    <td>
-                        {{ $task->file_size }}
-                    </td>
-                    <td>
-                        @if (sizeof($task->users))
-                            Booked
-                        @else
-                            Not Booked
-                        @endif
-                    </td>
-                    <td>
-                        <div class="d-flex justify-content-start">
-                            <a href="#" class="btn btn-primary me-2 @if(sizeof($task->users) > 0) disabled @endif">
-                                <i class="fas fa-ticket-alt"></i>
-                                Booking
-                            </a>
-                            <a href="#" class="btn btn-danger me-2 @if(sizeof($task->users) == 0) disabled @endif">
-                                <i class="fas fa-times"></i>
-                                Revoke
-                            </a>
-                        </div>
-                    </td>
-                    <td>
-                        <div class="d-flex justify-content-start">
-                            <a href="#" class="btn btn-primary me-2 @if(sizeof($task->users) == 0) disabled @endif">
-                                <i class="fas fa-download"></i>
-                                Download
-                            </a>
-                            <form action="#" method="POST">
-                                @csrf
-                                @method('DELETE')
-                                <button type="submit" class="btn btn-danger">
-                                    <i class="fas fa-trash"></i>
-                                    Delete
-                                </button>
-                            </form>
-                        </div>
-                    </td>
-                </tr>
-                @endforeach
-            </tbody>
-        </table>
+        <div class="table-responsive">
+            <table class="table table-borderless">
+                <thead>
+                    <tr class="bg-secondary text-white">
+                        <th scope="col">NO</th>
+                        <th scope="col">NAME</th>
+                        <th scope="col">SIZE</th>
+                        <th scope="col">STATUS</th>
+                        <th scope="col">BOOKING</th>
+                        <th scope="col">ACTION</th>
+                    </tr>
+                </thead>
+                <tbody class="bg-light">
+                    @foreach($tasks as $task)
+                    <tr class="align-middle">
+                        <td>
+                            {{ $loop->iteration }}
+                        </td>
+                        <td>
+                            <strong>{{ $task->name }}</strong>
+                        </td>
+                        <td>
+                            {{ $task->file_size }}
+                        </td>
+                        <td>
+                            @if (sizeof($task->users))
+                                Booked
+                            @else
+                                Not Booked
+                            @endif
+                        </td>
+                        <td>
+                            <div class="d-flex justify-content-start">
+                                <a href="#" class="btn btn-primary me-2 @if(sizeof($task->users) > 0) disabled @endif">
+                                    <i class="fas fa-ticket-alt"></i>
+                                    Booking
+                                </a>
+                                <a href="#" class="btn btn-danger me-2 @if(sizeof($task->users) == 0) disabled @endif">
+                                    <i class="fas fa-times"></i>
+                                    Revoke
+                                </a>
+                            </div>
+                        </td>
+                        <td>
+                            <div class="d-flex justify-content-start">
+                                <a href="#" class="btn btn-primary me-2 @if(sizeof($task->users) == 0) disabled @endif">
+                                    <i class="fas fa-download"></i>
+                                    Download
+                                </a>
+                                <form action="#" method="POST">
+                                    @csrf
+                                    @method('DELETE')
+                                    <button type="submit" class="btn btn-danger">
+                                        <i class="fas fa-trash"></i>
+                                        Delete
+                                    </button>
+                                </form>
+                            </div>
+                        </td>
+                    </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
     </div>
 
 @endsection
