89419099ba506aec47f2bfb42ecc1dc2dc5ebf8e
nangirvan <nangirvan98@gmail.com>
2021-02-20 17:15:57 +0700

    Dataset: Add pesan error pada form upload dataset

--- resources/views/task-management/create.blade.php
+++ resources/views/task-management/create.blade.php
@@ -2,7 +2,7 @@
 @section('content')
 
     <div class="mt-4">
-    <form action="{{ route('task-management.store') }}" method="POST">
+    <form action="{{ route('task-management.store') }}" method="POST" enctype="multipart/form-data">
         @csrf
         <div class="d-flex justify-content-between mb-4">
             <div>
@@ -16,9 +16,15 @@
         </div>
         <div class="mb-3">
             <label for="inAddTaskDataset" class="form-label">Dataset File</label>
-            <input type="file" class="form-control" id="inAddTaskDataset" name="dataset" required>
-            <div id="inAddTaskName" class="form-text">File must be a .zip format</div>
+            <input type="file" class="form-control @error('dataset') is-invalid @enderror" id="inAddTaskDataset" name="dataset" required>
+            <div id="inAddTaskName" class="form-text @error('dataset') d-none @enderror">File type must be a .zip</div>
+            @error('dataset')
+            <div class="invalid-feedback">
+                {{ $message }}
+            </div>
+            @enderror
         </div>
+        
         <button type="submit" class="btn btn-primary me-2">Submit</button>
         <a href="{{ route('home') }}" class="btn btn-danger">Cancel</a>
     </form>
