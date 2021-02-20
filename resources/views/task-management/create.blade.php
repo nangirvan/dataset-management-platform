90ba7b47e1dcdfddd66e1cab4eabc5dc32f4879b
nangirvan <nangirvan98@gmail.com>
2021-02-20 17:00:12 +0700

    Dataset: Edit layout add task untuk upload file

--- resources/views/task-management/create.blade.php
+++ resources/views/task-management/create.blade.php
@@ -6,18 +6,18 @@
         @csrf
         <div class="d-flex justify-content-between mb-4">
             <div>
-                <h4 class="text-secondary fw-bold">Add Task</h4>
+                <h4 class="text-secondary fw-bold">Upload Dataset</h4>
             </div>
         </div>
         <div class="mb-3">
-            <label for="inAddTaskName" class="form-label">Nama</label>
+            <label for="inAddTaskName" class="form-label">Name</label>
             <input type="text" class="form-control" id="inAddTaskName" name="name" value="{{ old('name') }}">
-            <div id="inAddTaskName" class="form-text">Kosongkan jika ingin menggunakan nama default file.</div>
+            <div id="inAddTaskName" class="form-text">Leave it empty if you want to use default name</div>
         </div>
         <div class="mb-3">
-            <label for="inAddTaskDataset" class="form-label">File Dataset</label>
-            <input type="text" class="form-control" id="inAddTaskDataset" name="dataset" required>
-            <div id="inAddTaskName" class="form-text">Hanya file dengan format .zip yang diperbolehkan.</div>
+            <label for="inAddTaskDataset" class="form-label">Dataset File</label>
+            <input type="file" class="form-control" id="inAddTaskDataset" name="dataset" required>
+            <div id="inAddTaskName" class="form-text">File must be a .zip format</div>
         </div>
         <button type="submit" class="btn btn-primary me-2">Submit</button>
         <a href="{{ route('home') }}" class="btn btn-danger">Cancel</a>
