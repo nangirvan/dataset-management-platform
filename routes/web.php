0ee80f376b9f55600121a8daee206951bd1e1d36
nangirvan <nangirvan98@gmail.com>
2021-02-20 10:12:35 +0700

    Auth: Add route

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,5 +15,11 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('task-management.index');
+})->middleware('auth');
+
+Route::prefix('auth')->name('auth.')->group(function() {
+    Route::get('/login', [AuthController::class, 'index'])->name('login.view');
+    Route::post('/login', [AuthController::class, 'login'])->name('login');
+    Route::get('/logout', [AuthController::class, 'logout'])->name('logout');
 });
