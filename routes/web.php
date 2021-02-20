8931447c30d3d05fb358c79679c94e84f1ac2ccf
nangirvan <nangirvan98@gmail.com>
2021-02-20 10:50:01 +0700

    Task: Add auth pada route

--- routes/web.php
+++ routes/web.php
@@ -15,14 +15,15 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::redirect('/', '/task-management')->name('home')->middleware('auth');
-
 Route::prefix('auth')->name('auth.')->group(function() {
     Route::get('/login', [AuthController::class, 'index'])->name('login.view');
     Route::post('/login', [AuthController::class, 'login'])->name('login');
     Route::get('/logout', [AuthController::class, 'logout'])->name('logout');
 });
 
-Route::resources([
-    '/task-management' => TaskManagementController::class,
-]);
+Route::middleware('auth')->group(function() {
+    Route::redirect('/', '/task-management')->name('home');
+    Route::resources([
+        '/task-management' => TaskManagementController::class,
+    ]);
+});
