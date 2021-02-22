7747e2e09cf0f52d3f70e409cdc775c3da3191a2
nangirvan <nangirvan98@gmail.com>
2021-02-22 11:10:07 +0700

    View: Remove jquery

--- resources/views/layouts/auth.blade.php
+++ resources/views/layouts/auth.blade.php
@@ -13,7 +13,6 @@
         @yield('content')
     </div>
 
-    <script src="https://code.jquery.com/jquery-3.5.1.js" integrity="sha256-QWo7LDvxbWT2tbbQ97B53yJnYU3WhH/C8ycbRAkjPDc=" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
     <script src="https://kit.fontawesome.com/60a6db6bb7.js" crossorigin="anonymous"></script>
 </body>
